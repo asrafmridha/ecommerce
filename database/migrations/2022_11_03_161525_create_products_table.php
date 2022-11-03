0dcbde694146212923de2f8ad785486ffc8b7143
asraf-mridha <asrafmridha@gmail.com>
2022-11-03 22:38:52 +0600

    Create Database Table for Product

--- database/migrations/2022_11_03_161525_create_products_table.php
+++ database/migrations/2022_11_03_161525_create_products_table.php
@@ -15,6 +15,22 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('vendor_id')->nullable();
+            $table->string('category_id')->nullable();
+            $table->string('subcategory_id')->nullable();
+            $table->string('product_name')->nullable();
+            $table->string('slug')->nullable();
+            $table->integer('product_code')->nullable();
+            $table->float('discount')->nullable();
+            $table->float('discount_price')->nullable();
+            $table->float('product_price')->nullable();
+            $table->longText('short_description')->nullable();
+            $table->longText('long_description')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('status')->nullable();
+            $table->integer('star')->nullable();
+            $table->string('image')->nullable();  
+            
             $table->timestamps();
         });
     }
